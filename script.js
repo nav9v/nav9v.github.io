1390c1e67a77adf7613ef09277b8425ded86b996
navneet <navneetsharma2302@gmail.com>
2024-04-18 22:54:54 +0530

    Update script.js and index.html

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-
 (function ($) {
-  $(window).on('load hashchange', function(){
+  $(window).on('load hashchange', function () {
     $('.content-region').hide();
     $('.main-menu a').removeClass('active');
     var region = location.hash.toString() || $('.main-menu a:first').attr('href');
     $(region).show();
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
+    $('.main-menu a[href="' + region + '"]').addClass('active');
   });
-  
+
 })(jQuery);
+function updateTime() {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  const timeString = hours + ':' + minutes + ':' + seconds;
+  document.getElementById('clock').textContent = timeString;
+
+  setTimeout(updateTime, 1000);
+}
+
+updateTime();
