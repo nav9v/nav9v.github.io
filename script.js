2bae85fcf300135aa46a58631e54b9a6cfca3b62
navneet <navneetsharma2302@gmail.com>
2024-07-25 16:19:53 +0530

    comressed and js (dom imp & requestAnimationFrame)

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 (function ($) {
-  $(window).on('load hashchange', function () {
-    $('.content-region').hide();
-    $('.main-menu a').removeClass('active');
-    var region = location.hash || '#home';
+  const $contentRegion = $('.content-region');
+  const $mainMenuLinks = $('.main-menu a');
+  function updateContent() {
+    $contentRegion.hide();
+    $mainMenuLinks.removeClass('active');
+    const region = location.hash || '#home';
     $(region).show();
-    $('.main-menu a[href="' + region + '"]').addClass('active');
-  });
-  $(window).trigger('load');
+    $mainMenuLinks.filter('[href="' + region + '"]').addClass('active');
+  }
+  $(window).on('load hashchange', updateContent).trigger('load');
+  
 })(jQuery);
 
 function updateTime() {
+  requestAnimationFrame(updateTime);
   const now = new Date();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
-
-  hours = hours < 10 ? '0' + hours : hours;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  seconds = seconds < 10 ? '0' + seconds : seconds;
-
-  const timeString = hours + ':' + minutes + ':' + seconds;
+  let hours = now.getHours().toString().padStart(2, '0');
+  let minutes = now.getMinutes().toString().padStart(2, '0');
+  let seconds = now.getSeconds().toString().padStart(2, '0');
+  const timeString = `${hours}:${minutes}:${seconds}`;
   document.getElementById('clock').textContent = timeString;
-
-  setTimeout(updateTime, 1000);
 }
 
-updateTime();
+updateTime(); 
